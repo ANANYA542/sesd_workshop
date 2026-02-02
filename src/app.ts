8bbfa873836f2ff73f485c209c8e668d42f82333
ANANYA542 <ananyashrma6512@gmail.com>
2026-02-02 22:52:43 +0530

    feat(app): Setup Express app and Server entry point

--- src/app.ts
+++ src/app.ts
@@ -1,48 +1,15 @@
-import express from "express";
-import { Routes } from "./utils/route.Interface";
-import { connect } from "mongoose";
+import express, { Application } from 'express';
+import bookRoutes from './routes/book.routes';
+import { errorHandler } from './middleware';
 
-class App {
-  public app: express.Application;
-  public port: string | number;
+const app: Application = express();
 
-  constructor(routes: Routes[]) {
-    this.app = express();
-    this.port = 8080;
-    this.initializeMiddlewares();
-    this.initializeRoutes(routes);
-    this.connectDatabase();
-  }
 
-  public startServer() {
-    this.app.listen(this.port, () => {
-      console.log(`Server listening on http://localhost:${this.port}`);
-    });
-  }
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-  private initializeRoutes(routes: Routes[]) {
-    routes.forEach((route) => {
-      this.app.use("/", route.router);
-    });
-  }
+app.use('/api/books', bookRoutes);
 
-  private initializeMiddlewares() {
-    this.app.use(express.json());
-  }
+app.use(errorHandler);
 
-  private async connectDatabase() {
-    const uri = process.env.MONGODB_URI;
-    if (!uri) {
-      throw new Error("MONGODB_URI is missing in environment variables");
-    }
-    try {
-      await connect(uri);
-      console.log("Database connected...");
-    } catch (err) {
-      console.error(err);
-      process.exit(1);
-    }
-  }
-}
-
-export default App;
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,20 @@
-import App from "./app";
-import TodoRoutes from "./routes/todo.routes";
-import "dotenv/config";
+import mongoose from 'mongoose';
+import app from './app';
+import dotenv from 'dotenv';
 
-const app = new App([new TodoRoutes()]);
-app.startServer();
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/book-crud';
+
+mongoose
+    .connect(MONGO_URI)
+    .then(() => {
+        console.log('Connected to MongoDB');
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.error('Database connection error:', error);
+    });
